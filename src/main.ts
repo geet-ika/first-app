2e6d172422e30bbba210e995231a11f7751337bc
Geetika Jain <jaingeetika8797@gmail.com>
2023-07-02 14:17:16 -0600

    Add routeConfig

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
-
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
 
 bootstrapApplication(AppComponent,
-  {providers: [provideProtractorTestingSupport()]})
+  {providers: [
+    provideProtractorTestingSupport(),
+    provideRouter(routeConfig)
+  ]})
   .catch(err => console.error(err));
